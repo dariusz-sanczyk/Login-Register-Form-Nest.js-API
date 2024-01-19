d9eb799bd20ca87d38108ae5e62d0de45e68842b
Darek Sańczyk <tantyk@gmail.com>
2024-01-19 20:49:45 +0100

    configuration and packages adjustments

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { LoginModule } from './modules/login/login.module';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoginModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/config/typeorm.config.ts
@@ -0,0 +1,28 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleOptions,
+  TypeOrmModuleAsyncOptions,
+} from '@nestjs/typeorm';
+
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+};
