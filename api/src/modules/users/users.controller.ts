621030885cccaaea4288414fae334f4707db1118
Darek Sańczyk <tantyk@gmail.com>
2024-01-23 17:07:54 +0100

    working on endpoints

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,16 +1,39 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
   @Get()
-  getUsers() {}
+  getUsers() {
+    return this.usersService.findAllUsers();
+  }
+
+  @Get('/:id')
+  getuserByID(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findUserById(id);
+  }
 
   @Post()
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.createUser(body);
-    return body;
+    return this.usersService.createUser(body);
+  }
+
+  @Patch('/:id')
+  async updateUser(
+    @Body() body: UpdateUserDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    await this.usersService.updateUser(id, body.password);
   }
 }
--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -3,16 +3,27 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User.entity';
 import { CreatUserParams } from 'src/utils/types';
 import { Repository } from 'typeorm';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
-  findAllUsers() {}
+  findAllUsers() {
+    return this.userRepository.find();
+  }
+
+  findUserById(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
 
   createUser(userCredentials: CreatUserParams) {
     const newUser = this.userRepository.create({ ...userCredentials });
     return this.userRepository.save(newUser);
+  }
+
+  updateUser(id: number, password: string) {
+    return this.userRepository.update({ id }, { password });
   }
 }
